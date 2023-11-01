f74e8076973b9f30d0223434a2454cecfe238e27
maryamrz402 <136293878+maryamrz402@users.noreply.github.com>
2023-11-01 10:41:55 +0330

    added Github URL repo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Maryam Reyhanian and is
+      <a 
+      href= "https://github.com/maryamrz402/weather-react-app"
+    >
+        open-sourced on GitHub
+        </a> 
+      </footer>
     </div>
   );
 }
