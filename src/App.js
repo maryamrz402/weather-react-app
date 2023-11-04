a9f189377b5bf05df0d22ea692cc4cbff7ef4acd
maryamrz402 <136293878+maryamrz402@users.noreply.github.com>
2023-11-04 08:41:45 +0330

    added bootstrap and html

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
+import Weather from "./Weather";
 import './App.css';
-
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+      
+      <Weather/>
       <footer>
         This project was coded by Maryam Reyhanian and is
       <a 
       href= "https://github.com/maryamrz402/weather-react-app"
     >
-        open-sourced on GitHub
+         open-sourced on GitHub
         </a> 
       </footer>
-    </div>
+    </div></div>
   );
 }
 
---
+++ src/Weather.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather(){
+return<div className="Weather">
+    <form>
+        <div className="row">
+           <div className="col-9">
+        <input type="search"
+         placeholder="Enter a City"
+         className="form-control"/></div>
+         <div className="col-3">
+        <input  type="submit" value="search"
+        className="btn btn-primary"/></div></div>
+    </form>
+    <h1>Tehran</h1>
+    <ul>
+        <li>wednesday 07:00</li>
+        <li>mostly cloudy</li>
+        </ul>
+        <div className="row">
+            <div className="col-6">
+                <img  src="https://ssl.gstatic.com/onebox/weather/64/fog.png"alt="mostly cloudy"/>
+                6°C
+            </div>
+            <div className="col-6">
+                <ul>
+                    <li>precipitation: 15%</li>
+                    <li>humidity: 72%</li>
+                    <li>wind: 7km/h</li>
+                </ul>
+            </div>
+        </div>
+</div>
+}
